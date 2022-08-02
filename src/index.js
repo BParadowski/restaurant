0f958e14ca3c07847f25c70a26fb1d366b99b97b
BParadowski <bartoszparadowski01@gmail.com>
2022-08-02 19:44:25 +0200

    Add fade in animation for all pages.

--- src/index.js
+++ src/index.js
@@ -7,16 +7,20 @@ const contentDiv = document.getElementById('content');
 const navHome = document.getElementById('home-button')
 const navMenu = document.getElementById('menu-button')
 const navContact = document.getElementById('contact-button')
+let currentPage;
 
 const displayHome = () => {
-    contentDiv.replaceChildren(homePage);
+    if (currentPage !== 'homePage') contentDiv.replaceChildren(homePage);
+    currentPage = 'homePage';
 }
 const displayMenu = () => {
-    contentDiv.replaceChildren(menuPage);
+    if (currentPage !== 'menuPage') contentDiv.replaceChildren(menuPage);
+    currentPage = 'menuPage';
 }
 
 const displayContactInfo = () => {
-    contentDiv.replaceChildren(contactPage);
+    if (currentPage !== 'contactPage') contentDiv.replaceChildren(contactPage);
+    currentPage = 'contactPage';
 }
 
 navHome.addEventListener('click', displayHome);
