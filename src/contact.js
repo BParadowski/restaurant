e4f279e2ece15fa17cfc88fbebabe7768ae25d9c
BParadowski <bartoszparadowski01@gmail.com>
2022-08-02 19:31:19 +0200

    Add contact info page

---
+++ src/contact.js
@@ -0,0 +1,8 @@
+let contactPage;
+export default contactPage = (function(){
+    const contact = document.createElement('div');
+    contact.classList.add('contact');
+    contact.textContent = "Phone number: 234 9628 90\r\nE-mail adress: Beluga@watercreatures.com"
+
+    return contact;
+})();
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import "./style.scss";
 import homePage from "./home.js";
-import menuPage from "./menu";
+import menuPage from "./menu.js";
+import contactPage from "./contact.js"
 
 const contentDiv = document.getElementById('content');
 const navHome = document.getElementById('home-button')
@@ -14,7 +15,13 @@ const displayMenu = () => {
     contentDiv.replaceChildren(menuPage);
 }
 
+const displayContactInfo = () => {
+    contentDiv.replaceChildren(contactPage);
+}
+
 navHome.addEventListener('click', displayHome);
 navMenu.addEventListener('click', displayMenu);
+navContact.addEventListener('click', displayContactInfo);
 
 displayHome();
+
